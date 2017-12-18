8f39b04e4e7650f5e16abe1941b32cd76d848158
Ludovic Sanctorum <lsa@opcoding.eu>
2017-12-18 14:46:40 +0100

    Improve scripts, prepare add assets'

--- scripts/ComposerScript.php
+++ scripts/ComposerScript.php
@@ -2,7 +2,8 @@
 
 namespace Opcoding\StarterKit\Tools;
 
-use Composer\Script\Event;
+use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
 
 /**
  * Class InstallAssets
@@ -11,17 +12,31 @@ use Composer\Script\Event;
  */
 class ComposerScript
 {
-    /**
-     * @param Event $event
-     *
-     * @throws \Exception
-     */
-    public static function exec(Event $event)
-    {
-        $io = $event->getIO();
 
-        $io->write(sprintf('<comment>Start configuration for composer.json information</comment>'));
-        $composerProjectName = Helper::createQuestion("Project name (Example:  foo/bar) : ", function($result) use ($io){
+    /** @var IOInterface io */
+    protected $io;
+
+    /** @var JsonFile */
+    protected $jsonFile;
+
+    /**
+     * ComposerScript constructor.
+     */
+    public function __construct(IOInterface $io, JsonFile $jsonFile)
+    {
+        $this->io = $io;
+        $this->jsonFile = $jsonFile;
+    }
+
+    /**
+     * @return array
+     */
+    private function getDataForComposer()
+    {
+        $io = $this->io;
+
+        $io->write(sprintf('<comment>Set composer.json information (project name, description, namespace psr-4)</comment>'));
+        $name = StarterKit::createQuestion("Project name (Example: foo/bar) : ", function($result) use ($io){
             $res = strtolower($result);
             if (preg_match('/^[a-z0-9-]+\/[a-z0-9-]+$/', $res) === 1) {
                 return $res;
@@ -30,15 +45,7 @@ class ComposerScript
             return false;
         }, $io);
 
-        $composerProjectDescription = Helper::createQuestion("Project description : ", function($result) use ($io){
-            $res = trim($result);
-            if (!empty($res)) {
-                return $res;
-            }
-            return false;
-        }, $io);
-
-        $projectName = Helper::createQuestion("Project name (application name) : ", function($result) use ($io){
+        $description = StarterKit::createQuestion("Project description : ", function($result) use ($io){
             $res = trim($result);
             if (!empty($res)) {
                 return $res;
@@ -47,7 +54,7 @@ class ComposerScript
         }, $io);
 
         $io->write(sprintf('<comment>Namespace format: My\\\Super\\\Namespace</comment>'));
-        $namespace = Helper::createQuestion("Default namespace for autoload : ", function($result) use ($io){
+        $namespace = StarterKit::createQuestion("Default namespace for autoload : ", function($result) use ($io){
             if (preg_match('/^((\w+)|(?:\w+\\\\{2}\w+)+)$/', $result) === 1) {
                 return $result;
             }
@@ -55,16 +62,56 @@ class ComposerScript
             return false;
         }, $io);
 
+        return [
+            'name' => $name,
+            'description' => $description,
+            'autoload' => [
+                'psr-4' => [
+                    $namespace => 'app/src'
+                ]
+            ],
+            'property-file' => [
+                'namespace' => $namespace
+            ]
+        ];
+    }
+
+    private function setGlobalPropertiesFile($data)
+    {
+        $io = $this->io;
+
+        $projectName = StarterKit::createQuestion("Project name (application name) : ", function($result) use ($io){
+            $res = trim($result);
+            if (!empty($res)) {
+                return $res;
+            }
+            return false;
+        }, $io);
+
+        $namespace = $data['property-file']['namespace'];
+
         $data = sprintf(
             'namespace.project=%s
              namespace.class.escape=%s
              project.name=%s
-             composer.project.name=%s
-             composer.project.description=%s
-            ', $namespace, str_replace("\\\\", '\\', $namespace), $projectName, $composerProjectName, $composerProjectDescription);
-        file_put_contents(Helper::VENDOR_PATH . 'phing/properties/global.properties', $data);
+            ', $namespace, str_replace("\\\\", '\\', $namespace), $projectName);
+        file_put_contents(StarterKit::VENDOR_PATH . 'phing/properties/global.properties', $data);
+    }
 
-        shell_exec("vendor/bin/phing -debug -f vendor/opcoding/starter-kit-tools/build.xml main");
-        $io->write(sprintf('<info>Default namespace successfully set</info>'));
+    /**
+     * Update composer.json
+     */
+    public function updateComposerJson()
+    {
+        /** @var array $composerJsonData */
+        $composerJsonData = $this->jsonFile->read();
+        $newData = $this->getDataForComposer();
+
+        $this->setGlobalPropertiesFile($newData['property-file']);
+        unset($newData['property-file']);
+
+        array_merge($composerJsonData, $newData);
+
+        $this->jsonFile->write(array_merge($composerJsonData, $newData));
     }
 }
---
+++ scripts/StarterKit.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Opcoding\StarterKit\Tools;
+
+use Composer\Composer;
+use Composer\Factory;
+use Composer\IO\IOInterface;
+use Composer\Json\JsonFile;
+use Composer\Script\Event;
+use Exception;
+
+/**
+ * Class StarterKit
+ *
+ * @package Opcoding\StarterKit\Tools
+ */
+class StarterKit
+{
+    const VENDOR_PATH = "vendor/opcoding/starter-kit-tools/";
+
+    /** @var string */
+    protected $projectRoot;
+
+    /** @var IOInterface */
+    protected $io;
+
+    /** @var Composer */
+    protected $composer;
+
+    /** @var JsonFile */
+    protected $composerJson;
+
+    /**
+     * StarterKit constructor.
+     *
+     * @param IOInterface $io
+     * @param Composer    $composer
+     */
+    public function __construct(IOInterface $io, Composer $composer)
+    {
+        $this->projectRoot = realpath(dirname(Factory::getComposerFile()));
+        $this->io = $io;
+        $this->composer = $composer;
+        $this->composerJson = new JsonFile($this->projectRoot . '/composer.json');
+    }
+
+    /**
+     * @param Event $event
+     *
+     * @throws Exception
+     */
+    static public function init(Event $event)
+    {
+        $starterKit = new self($event->getIO(), $event->getComposer());
+
+        $starterKit->setComposer($starterKit->io, $starterKit->composerJson);
+        $starterKit->setPhpConfig();
+
+        //end
+        $starterKit->clearComposerJson();
+    }
+
+    /**
+     * @param IOInterface $io
+     * @param JsonFile    $composerJson
+     */
+    private function setComposer(IOInterface $io, JsonFile $composerJson)
+    {
+        (new ComposerScript($io, $composerJson))
+            ->updateComposerJson();
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function setPhpConfig()
+    {
+        if (!file_exists(self::VENDOR_PATH . 'phing/properties/global.properties')) {
+            throw new Exception("The file global.properties doesn't exists");
+        }
+
+        shell_exec("vendor/bin/phing -debug -f vendor/opcoding/starter-kit-tools/build.xml defaultFile");
+        $this->io->write(sprintf('<info>Namespaces updated</info>'));
+    }
+
+    /**
+     * Remove post-create-project-cmd
+     */
+    private function clearComposerJson()
+    {
+        $data = $this->composerJson->read();
+
+        unset($data['scripts']['post-create-project-cmd']);
+
+        $this->composerJson->write($data);
+    }
+
+    /**
+     * @param $question
+     * @param $validator
+     * @param $io
+     *
+     * @return string
+     */
+    static public function createQuestion($question, $validator, $io)
+    {
+        $response = false;
+
+        while (empty($response)) {
+            $response = $io->askAndValidate(sprintf('<question>%s</question>',$question), $validator);
+        }
+
+        return $response;
+    }
+}
